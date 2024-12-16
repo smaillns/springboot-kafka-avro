0e14b934594248e8ae353389277b57c59ff304ad
Smail <smail.lounes.partner@decathlon.com>
2024-12-16 07:16:16 +0100

    added topic name to application.yml

--- src/main/java/com/example/demo/springbootkafkaavro/consumer/MyKafkaConsumer.java
+++ src/main/java/com/example/demo/springbootkafkaavro/consumer/MyKafkaConsumer.java
@@ -1,8 +1,9 @@
-package com.example.demo.springbootkafkaavro;
+package com.example.demo.springbootkafkaavro.consumer;
 
 import com.example.demo.springbootkafkaavro.event.Event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MyKafkaConsumer {
 
+
+    @Value("${app.kafka.topic}")
+    private String topic;
 
     @KafkaListener(topics = "my_topic", groupId = "groupId")
     public void consumePaymentEvents(Event event)  {
